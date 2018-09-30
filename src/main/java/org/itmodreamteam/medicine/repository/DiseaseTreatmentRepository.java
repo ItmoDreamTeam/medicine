eaf9db5558ed2d574646387876e8621b3b489a41
Kirill Smirnov <smirnowku@gmail.com>
2018-09-30 20:31:40 +0300

    Implement TreatmentRunner

--- src/main/java/org/itmodreamteam/medicine/repository/DiseaseTreatmentRepository.java
+++ src/main/java/org/itmodreamteam/medicine/repository/DiseaseTreatmentRepository.java
@@ -1,6 +1,11 @@
 package org.itmodreamteam.medicine.repository;
 
+import org.itmodreamteam.medicine.model.disease.Disease;
 import org.itmodreamteam.medicine.model.disease.DiseaseTreatment;
 
+import java.util.List;
+
 public interface DiseaseTreatmentRepository extends BaseRepository<DiseaseTreatment> {
+
+    List<DiseaseTreatment> findAllByDiseaseOrderBySequentialNumber(Disease disease);
 }
